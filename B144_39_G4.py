9d66a3c6c984a742207cc0585ab179f5c60da13d
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2024-07-18 12:24:30 +0000

    test

---
+++ B144_39_G4.py
@@ -0,0 +1,14 @@
+import pandas as pd  
+
+
+# Specify the path to your CSV file  
+file_path = '/workspaces/clean_code/sample_csv.csv'  
+  
+# Load the CSV file into a DataFrame  
+df = pd.read_csv(file_path)  
+  
+# Explore the DataFrame  
+print(df.head()) # View the first few rows  
+print(df.info()) # Get summary information about the DataFrame  
+  
+# Further processing or analysis can be done with the DataFrame  
